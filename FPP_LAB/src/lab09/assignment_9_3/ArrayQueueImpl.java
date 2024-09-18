6ba5cd7632e23cec83adc8dd629aa045b3cdea93
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2024-09-18 16:45:10 -0500

    finish implement assignment_9_3

--- FPP_LAB/src/lab09/assignment_9_3/ArrayQueueImpl.java
+++ FPP_LAB/src/lab09/assignment_9_3/ArrayQueueImpl.java
@@ -10,15 +10,68 @@ public class ArrayQueueImpl {
 		return size == 0;
 	}
 
+	public int size() {
+		return size;
+	}
+
+	public void enqueue(int value) {
+		arr[rear] = value;
+		rear++;
+		size++;
+		if (front == -1) {
+			front = 0;
+		}
+		if (rear == arr.length) {
+			resize();
+		}
+	}
+
+	public int dequeue() {
+		int res = arr[front];
+		for (int i = front; i < size - 1; i++) {
+			arr[i] = arr[i + 1];
+		}
+		size--;
+		rear--;
+
+		return res;
+	}
+
+	public int peek() {
+		if (isEmpty()) {
+			throw new IllegalStateException("Cannot peak becasue the queue is empty!");
+		}
+		return arr[front];
+	}
+
+	private void resize() {
+		System.out.println("resize ... ");
+		int currentLength = arr.length;
+		int newLength = 2 * currentLength;
+		int[] temp = arr;
+		arr = new int[newLength];
+		System.arraycopy(temp, 0, arr, 0, currentLength);
+	}
+
 	public static void main(String[] args) {
 		ArrayQueueImpl q = new ArrayQueueImpl();
 
 		// uncomment when ready
-//		for(int i = 0; i < 15; i ++)
-//			q.enqueue(i);
-//		for(int i = 0; i < 14; i ++)
-//			q.dequeue();
-//		System.out.println(q.size());
-//		System.out.println(q.peek());
+		for (int i = 0; i < 15; i++) {
+			q.enqueue(i);
+		}
+		for (int i = 0; i < 14; i++) {
+			q.dequeue();
+		}
+		System.out.println(q.size());
+		System.out.println(q.peek());
+		
+		q.dequeue();
+		try {
+			q.peek();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
