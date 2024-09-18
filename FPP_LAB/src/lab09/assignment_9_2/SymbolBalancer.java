32eba20f634d6032aaa549b3b8e3f9e6fe9aef11
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2024-09-18 16:00:59 -0500

    finish implement assignment_9_2

--- FPP_LAB/src/lab09/assignment_9_2/SymbolBalancer.java
+++ FPP_LAB/src/lab09/assignment_9_2/SymbolBalancer.java
@@ -21,15 +21,47 @@ public class SymbolBalancer {
 		this.text = text;
 	}
 
-	boolean symbolsBalanced(String delimiters) {
+	public boolean symbolsBalanced(String delimiters) {
 		// implement
-		return false;
+		Stack<Character> stack = new Stack<>();
+		String openChars = "{[(<";
+		for (int i = 0; i < text.length(); i++) {
+			char chr = text.charAt(i);
+			if (delimiters.indexOf(chr) == -1) {
+				continue;
+			}
+			
+			if (openChars.indexOf(chr) != -1) {
+				stack.push(chr);
+			}
+			else {
+				Object obj = stack.pop();
+				if (obj == null) {
+					return false;
+				}
+				if (obj.equals('{') && chr != '}' ||
+					obj.equals('[') && chr != ']' ||
+					obj.equals('(') && chr != ')' ||
+					obj.equals('<') && chr != '>' ||
+					obj.equals('|') && chr != '|') {
+					
+					return false;
+				}
+			}
+		}
+		
+		return stack.isEmpty() ? true: false;
 
 	}
 
 	public static void main(String[] args) {
-		SymbolBalancer sb = new SymbolBalancer("prog9_2_startup\\Employee.java");
-		System.out.println(sb.symbolsBalanced("[]{}<>()"));
+		SymbolBalancer sb = new SymbolBalancer("lab09\\assignment_9_2\\Employee.java");
+		System.out.println("Check balance for []{}<>()|| " + sb.symbolsBalanced("[]{}<>()||"));
+		System.out.println("Check balance for [] " + sb.symbolsBalanced("[]"));
+		System.out.println("Check balance for {} " + sb.symbolsBalanced("{}"));
+		System.out.println("Check balance for () " + sb.symbolsBalanced("()"));
+		System.out.println("Check balance for <> " + sb.symbolsBalanced("<>"));
+		System.out.println("Check balance for || " + sb.symbolsBalanced("||"));
 	}
 
 	void readFile() {
@@ -38,7 +70,7 @@ public class SymbolBalancer {
 			Scanner sc = new Scanner(new File(prefix + filename));
 			sc.useDelimiter(ODD_DELIMITER);
 			text = sc.next();
-			System.out.println(text);
+//			System.out.println(text);
 			sc.close();
 		} catch (FileNotFoundException ex) {
 			System.err.println("File Not Found Exception " + ex.getMessage());
