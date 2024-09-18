e216d7e7d1423a4959212adbb66ea750ce9fd66b
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2024-09-17 19:55:10 -0500

    finish assignmen_8_3

---
+++ FPP_LAB/src/lab08/assignment_8_3/employeesort/HireDateComparator.java
@@ -0,0 +1,27 @@
+package lab08.assignment_8_3.employeesort;
+
+import java.util.Comparator;
+import java.util.Date;
+
+public class HireDateComparator implements Comparator<Employee> {
+
+	@Override
+	public int compare(Employee e1, Employee e2) {
+		String name1 = e1.getName();
+		String name2 = e2.getName();
+		Date hireDate1 = e1.getHireDate();
+		Date hireDate2 = e2.getHireDate();
+		int salary1 = e1.getSalary();
+		int salary2 = e2.getSalary();
+		
+		if (hireDate1.compareTo(hireDate2) != 0) {
+			return hireDate1.compareTo(hireDate2);
+		}
+		if (name1.compareTo(name2) != 0) {
+			return name1.compareTo(name2);
+		}
+		return salary1 - salary2;
+		
+	}
+
+}
