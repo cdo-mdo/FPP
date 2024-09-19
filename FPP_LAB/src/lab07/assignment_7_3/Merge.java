cf6ea535c52c7e41044cf8828adaf9a462cee5af
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2024-09-19 13:28:59 -0500

    format code of lab07

--- FPP_LAB/src/lab07/assignment_7_3/Merge.java
+++ FPP_LAB/src/lab07/assignment_7_3/Merge.java
@@ -1,4 +1,4 @@
-package prog7_prob3;
+package lab07.assignment_7_3;
 
 import java.util.LinkedList;
 
@@ -8,18 +8,25 @@ import java.util.LinkedList;
  */
 public class Merge {
 	public LinkedList<Integer> merge(LinkedList<Integer> list1, LinkedList<Integer> list2) {
-		//implement
+		// implement
 		return null;
 	}
-	//test your code
+
+	// test your code
 	public static void main(String[] args) {
 		LinkedList<Integer> list1 = new LinkedList<>();
 		LinkedList<Integer> list2 = new LinkedList<>();
-		list1.add(4);list1.add(5);list1.add(21);list1.add(25);
-		list2.add(2);list2.add(9);list2.add(17);
+		list1.add(4);
+		list1.add(5);
+		list1.add(21);
+		list1.add(25);
+		list2.add(2);
+		list2.add(9);
+		list2.add(17);
 		Merge m = new Merge();
 		LinkedList list3 = m.merge(list1, list2);
-		if(list3 != null) System.out.println(list3);
-		
+		if (list3 != null)
+			System.out.println(list3);
+
 	}
 }
