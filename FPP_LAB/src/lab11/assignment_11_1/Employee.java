97861cf2407e76e1095dbd3f34d43564607c937b
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2024-09-20 21:28:47 -0500

    format code of lab11

--- FPP_LAB/src/lab11/assignment_11_1/Employee.java
+++ FPP_LAB/src/lab11/assignment_11_1/Employee.java
@@ -7,39 +7,45 @@ public class Employee {
 	private String firstName;
 	private String lastName;
 	private HashMap salaryRecord;
-	
+
 	public void addEntry(String date, double salary) {
-		//implement
+		// implement
 	}
+
 	public void printPaymentAmount(String date) {
-		//implement
-		
+		// implement
+
 	}
+
 	public void printAveragePaycheck() {
-		//implement
+		// implement
 	}
-	
+
 	public static void main(String[] args) {
 		Employee e = new Employee();
 		e.setFirstName("Jim");
 		e.setLastName("Jones");
-		for(int i = 1; i <= 12; ++i) {
-			e.addEntry(i+"/15/2011", 3070+5*i);
+		for (int i = 1; i <= 12; ++i) {
+			e.addEntry(i + "/15/2011", 3070 + 5 * i);
 		}
 		e.printPaymentAmount("3/15/2011");
 		e.printPaymentAmount("5/15/2010");
 		e.printAveragePaycheck();
-		
+
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
