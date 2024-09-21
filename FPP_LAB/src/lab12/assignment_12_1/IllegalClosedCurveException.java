1e9505c804a4294d87715aceeeb84011b0876d2b
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2024-09-21 14:48:17 -0500

    finish implement assignment_12_1

---
+++ FPP_LAB/src/lab12/assignment_12_1/IllegalClosedCurveException.java
@@ -0,0 +1,20 @@
+package lab12.assignment_12_1;
+
+public class IllegalClosedCurveException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 372063491301222129L;
+
+	public IllegalClosedCurveException() {
+		super();
+	}
+	
+	public IllegalClosedCurveException(Throwable t) {
+		super(t);
+	}
+	
+	public IllegalClosedCurveException(String msg) {
+		super(msg);
+	}
+}
---
+++ FPP_LAB/src/lab12/assignment_12_1/IllegalTriangleException.java
@@ -0,0 +1,22 @@
+package lab12.assignment_12_1;
+
+public class IllegalTriangleException extends IllegalClosedCurveException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2140671193591786599L;
+
+	public IllegalTriangleException() {
+		super();
+	}
+
+	public IllegalTriangleException(Throwable t) {
+		super(t);
+	}
+
+	public IllegalTriangleException(String msg) {
+		super(msg);
+	}
+
+}
